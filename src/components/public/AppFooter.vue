b716f84ce38d9c2e2363c98c34a877ed34dd7865
tolerious <tolerious@qq.com>
2017-11-23 15:50:54 +0800

    complete app footer.

--- src/components/public/AppFooter.vue
+++ src/components/public/AppFooter.vue
@@ -1,8 +1,32 @@
 <template>
-    <div class="">
+  <div class="app-footer">
+    <div class="inner">
+      <div class="container">
+        <span>首页</span>
+        <span>|</span>
+        <span>工程数据库</span>
+        <span>|</span>
+        <span>工程与工程师</span>
+        <span>|</span>
+        <span>版权贸易</span>
+        <span>|</span>
+        <span>工程技术论坛</span>
+        <span>|</span>
+        <span>同济设计在线</span>
+        <span>|</span>
+        <span>BIM培训中心</span>
+      </div>
+      <div class="copyright">
+        <span>Copyright &copy; 2017 Tongji. All Rights Reserved.</span>
+      </div>
+      <div class="contact">
+        <span>同济大学出版社版权所有 上海市杨浦区赤峰路2号 021-0000000</span>
+      </div>
     </div>
+  </div>
 </template>
-<style scoped>
+<style lang="scss" scoped>
+  @import "../../assets/css/public/appFooter.scss";
 </style>
 <script>
   export default {
--- src/components/search/searchIndex.vue
+++ src/components/search/searchIndex.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="searchIndex">
+    <search-component></search-component>
     <router-view name="leftPanel"></router-view>
     <router-view name="middlePanel"></router-view>
     <router-view name="middlePanel"></router-view>
@@ -11,6 +12,7 @@
 
 <script>
   import { getCookie, checkLoginCookie, deleteCookie } from '../../assets/js/cookie'
+  import searchComponent from '../public/searchComponent.vue'
 
   export default {
     mounted: function () {
@@ -20,7 +22,9 @@
       return {}
     },
     props: {},
-    components: {},
+    components: {
+      searchComponent,
+    },
     methods: {},
     computed: {},
   }
