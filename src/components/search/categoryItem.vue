fe3efa038a4ee129ae2b30005d381bb36ddf9d86
tolerious <tolerious@qq.com>
2017-11-23 20:45:54 +0800

    update code ...

---
+++ src/components/search/categoryItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="category-item">
+      <div class="level-1-category">
+        <div class="inner">
+          <span>所有类别</span>
+        </div>
+      </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+  @import '../../assets/css/search/categoryItem.scss'
+</style>
+<script>
+  export default {
+//    props:['levelOneCategoryName','levelTwoCategoryList']
+    mounted: function () {
+
+    },
+    components: {},
+    data () {
+      return {}
+    },
+    methods: {},
+    computed: {},
+    filters: {}
+
+  }
+</script>
--- src/components/search/leftPanel.vue
+++ src/components/search/leftPanel.vue
@@ -1,61 +1,26 @@
 <template>
-  <div class="">
-    <div class="list_left">
-      <div class="class_title"><span>所有类别</span></div>
-      <div class="list_class" @click="categorySearch(i)" v-for="i in categoryList">
-        <div class="list_title">
-          <p>{{i.name}}</p>
-          <!--<span><img src="../../assets/images/jian.png" alt=""></span>-->
-          <span style="">{{i.count}}</span>
-
-        </div>
-        <!--<ul v-for="child in category.children">-->
-        <!--<li><em>213</em><a v-text="child.name"></a></li>-->
-        <!--</ul>-->
-      </div>
-      <!--<div class="list_class">-->
-      <!--<div class="list_title">-->
-      <!--<p>分类三</p>-->
-      <!--<span><img src="../../assets/images/jia.png" alt=""></span>-->
-      <!--</div>-->
-      <!--<ul class="dn">-->
-      <!--<li><em>213</em><a href="">小分类三</a></li>-->
-      <!--<li><em>213</em><a href="">小分类三</a></li>-->
-      <!--<li><em>213</em><a href="">小分类三</a></li>-->
-      <!--<li><em>213</em><a href="">小分类三</a></li>-->
-      <!--</ul>-->
-      <!--</div>-->
-    </div>
-
+  <div class="left-panel">
+    <category-item></category-item>
   </div>
 </template>
-<style scoped>
+<style lang="scss" scoped>
+  @import '../../assets/css/search/leftPanel.scss'
 </style>
 <script>
+  import categoryItem from './categoryItem.vue'
+
   export default {
     mounted: function () {
 
     },
-    components: {},
+    components: {
+      categoryItem,
+    },
     data () {
       return {}
     },
-    methods: {
-      categorySearch: function (i) {
-        let p = {
-          levelOneCategory: i.nickName,
-
-        }
-      }
-    },
-    computed: {
-      categoryList: function () {
-        return this.$store.state.leftPanel.levelOneCategoryList
-      },
-      currentLevelOneCategory: function () {
-
-      }
-    },
+    methods: {},
+    computed: {},
     filters: {}
 
   }
