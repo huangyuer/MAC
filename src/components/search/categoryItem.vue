fba2d75a83abe35ed60fd94eff2becc8a7e3c33e
tolerious <tolerious@qq.com>
2017-11-24 08:41:46 +0800

    Update code ...

--- src/components/search/categoryItem.vue
+++ src/components/search/categoryItem.vue
@@ -1,14 +1,17 @@
 <template>
-    <div class="category-item">
-      <div class="level-1-category">
-        <div class="inner">
-          <span>所有类别</span>
-        </div>
+  <div class="category-item">
+    <div class="level-1-category">
+      <div class="inner">
+        <span>所有类别</span>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-jia"></use>
+        </svg>
       </div>
     </div>
+  </div>
 </template>
 <style lang="scss" scoped>
-  @import '../../assets/css/search/categoryItem.scss'
+  @import '../../assets/css/search/categoryItem.scss';
 </style>
 <script>
   export default {
