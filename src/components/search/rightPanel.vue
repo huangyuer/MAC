803a3255e30db1f90463c35ce56fc20a9c133a70
tolerious <tolerious@qq.com>
2017-11-27 22:48:38 +0800

    tag cloud complete.

--- src/components/search/rightPanel.vue
+++ src/components/search/rightPanel.vue
@@ -4,10 +4,13 @@
       <span>热搜词汇</span>
     </div>
     <div class="tag-cloud">
-      <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="140" cy="140" r="140" stroke="black" stroke-width="0" fill="red">
-        </circle>
-        <text v-for="i in textTags" :font-size='14 || height / 20 * (600 / (600-i.z))' :fill-opacity='((400+i.z)/600)' :x="i.x" :y="i.y">{{i.name}}</text>
+      <svg id="svvg" width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg"
+           @mousemove='listener($event)'>
+        <!--<circle :x="140" :y="140" :r="r" stroke="black" stroke-width="0" fill="white"></circle>-->
+        <text style="cursor: pointer" v-for="i in textTags"
+              :font-size='11>height / 20 * (600 / (600-i.z))?14:height / 20 * (600 / (600-i.z))'
+              :fill-opacity='((400+i.z)/600)' :x="i.x" :y="i.y" :z="i.z">{{i.text}}
+        </text>
       </svg>
     </div>
   </div>
@@ -18,8 +21,10 @@
 <script>
   export default {
     mounted: function () {
-      this.CX = parseInt(getComputedStyle(document.querySelector('svg')).width) / 2
-      this.CY = parseInt(getComputedStyle(document.querySelector('svg')).height) / 2
+//      this.CX = parseInt((document.getElementById('svvg').width) / 2)
+//      this.CY = parseInt((document.getElementById('svvg').height) / 2)
+      this.CX = 140
+      this.CY = 140
       this.getTags()
     },
     components: {},
@@ -27,18 +32,45 @@
       return {
         tags: [
           {
-            name: '机智云',
+            name: '詹天佑',
+          },
+          {
+            name: '郦道元',
+          },
+          {
+            name: '工程师史',
+          },
+          {
+            name: '地下空间',
+          },
+          {
+            name: '同济大学',
+          },
+          {
+            name: '建筑工程',
+          }, {
+            name: '城市轨道',
           },
           {
             name: '青云',
-          }
+          }, {
+            name: '青云',
+          },
+          {
+            name: '青云',
+          },
+          {
+            name: '青云',
+          },
+
         ],
         textTags: [],
         speedX: Math.PI / 360,
         speedY: Math.PI / 360,
         CX: 0,
         CY: 0,
-        r: 140,
+        r: 100,
+        height: 300
       }
     },
     methods: {
@@ -54,17 +86,15 @@
           tag.x = this.CX + this.r * Math.sin(a) * Math.cos(b)
           tag.y = this.CY + this.r * Math.sin(a) * Math.sin(b)
           tag.z = this.r * Math.cos(a)
-          tag.href = `/tags?tag=${tag.text}`
-          console.log(tag)
           tags.push(tag)
         }
         this.textTags = tags
       },
       rotate: function () {
-        setInterval(()=>{
+        setInterval(() => {
           this.rotateX(this.speedX)
           this.rotateY(this.speedY)
-        },170)
+        }, 30)
       },
       rotateX (angleX) {
         var cos = Math.cos(angleX)
@@ -85,6 +115,12 @@
           tag.x = x1
           tag.z = z1
         }
+      },
+      listener (event) {
+        var x = event.clientX - this.CX
+        var y = event.clientY - this.CY
+        this.speedX = x * 0.0001 > 0 ? Math.min(this.r * 0.00002, x * 0.0001) : Math.max(-this.r * 0.00002, x * 0.0001)
+        this.speedY = y * 0.0001 > 0 ? Math.min(this.r * 0.00002, y * 0.0001) : Math.max(-this.r * 0.00002, y * 0.0001)
       },
     },
     computed: {},
