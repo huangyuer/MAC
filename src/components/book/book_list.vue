211f56ce1c768da0fed42553f1d1a1d8c4a786cb
Leo Cai <leo@LeodeMacBook-Pro.local>
2017-12-05 15:03:27 +0800

    图书列表页样式修改

--- src/components/book/book_list.vue
+++ src/components/book/book_list.vue
@@ -1,46 +1,23 @@
 <template>
-  <div class="bg"> 
+  <div class="bg">
     <div class="clear"></div>
      <search-component></search-component>
     <div class="clear"></div>
     <div class="list_main">
       <div class="main_left">
-        <div class="list_left">
-          <div class="class_title"><span>图书库</span></div>
-          <div class="list_class"> 
-           <div class="list_title"  v-for="lib in sublibs"> 
-            <p @click="listBooksByLib(lib.name)" v-text="lib.name"></p>
-             <span><img src="../../assets/images/jian.png" alt=""></span> 
-          </div>  
-          </div> 
-          <div class="class_title"><span>学科分类</span></div>
 
-          <div class="list_class" v-for="category in categories">
-            <div class="list_title">
-              <p @click="listBooksByCategory(category.name)" v-text="category.name"></p>
-              <span><img src="../../assets/images/jian.png" alt=""></span>
-            </div>
-          </div> 
+        <list-left></list-left>
 
-          <div class="class_title"><span>中图分类</span></div>
-
-          <div class="list_class" v-for="clc in clcs">
-            <div class="list_title">
-              <p @click="listBooksByClc(clc.name)" v-text="clc.name"></p>
-              <span><img src="../../assets/images/jian.png" alt=""></span>
-            </div>
-          </div> 
-        </div>
         <div class="list_cent">
           <div class="list_list">
-            <div class="list_cout">已为您找到"<font color="#4a91e3">中文</font>"相关结果约{{booksTotal}}个</div>
+            <div class="list_cout">找到&nbsp;<font color="#E37423">{{booksTotal}}</font>&nbsp;条结果</div>
             <div class="list_li">
               <dl v-for="book in books">
                 <dt><img :src="url + book.cover" alt="" class="list_li_img"></dt>
                 <dd>
                   <em v-text="book.bindingFormat">图书</em>
                   <p>
-                    <router-link to="/book_info">
+                    <router-link to="/book/info">
                       <a v-text="book.name"></a>
                     </router-link>
                   </p>
@@ -67,20 +44,20 @@
       </div>
     </div>
     <div class="clear"></div>
-    
+
   </div>
 </template>
 
 <script>
   import searchComponent from '../public/searchComponent.vue'
-
+  import listLeft from '../book/list_left.vue'
 //  import searchBar from '../public/searchComponent.vue'
   import pagination from '../public/pagination.vue'
   import {errorHandle} from '../../assets/js/common'
   export default {
     name: 'book_list',
     data () {
-      return {  
+      return {
         total: 0,
         limit: 10,
         url: 'http://118.178.238.202:9988/'
@@ -91,106 +68,80 @@
     components: {
       searchComponent,
 //      searchBar,
-      pagination
+      pagination,
+      listLeft
     },
-    mounted: function() {  
-      this.getLatestBooks(this.currentCategory,this.currentClc, this.currentLib,this.pageSize, this.currentPage); 
-      // 获取子库列表
-      this.$store.dispatch('listSublibs', {}); 
-      // 获取学科分类
-      this.$store.dispatch('listSubjects', {}); 
-      // 获取中图分类
-      this.$store.dispatch('listClcs', {}); 
+    mounted: function() {
+      this.getLatestBooks(this.currentCategory,this.currentClc, this.currentLib,this.pageSize, this.currentPage);
     },
     watch:{
       currentCategory: {
-        handler: function (val, oldVal) {  
+        handler: function (val, oldVal) {
           this.getLatestBooks(val, this.currentClc, this.currentLib, this.pageSize, this.currentPage);
         }
       },
       currentClc: {
-        handler: function (val, oldVal) {  
-          this.getLatestBooks(this.currentCategory, val, this.currentLib, this.pageSize, this.currentPage); 
+        handler: function (val, oldVal) {
+          this.getLatestBooks(this.currentCategory, val, this.currentLib, this.pageSize, this.currentPage);
         }
       },
       currentLib: {
-        handler: function (val, oldVal) {  
-          this.getLatestBooks(this.currentCategory, this.currentClc, val, this.pageSize, this.currentPage); 
+        handler: function (val, oldVal) {
+          this.getLatestBooks(this.currentCategory, this.currentClc, val, this.pageSize, this.currentPage);
         }
-      }
+      },
     },
     computed: {
-      books () { 
-        return this.$store.getters.books;  
+      books () {
+        return this.$store.getters.books;
       },
-      isSearchResult () { 
-        return this.$store.getters.books.isSearchResult;  
+      isSearchResult () {
+        return this.$store.getters.books.isSearchResult;
       },
       booksTotal(){
         return this.$store.getters.booksTotal;
       },
-      sublibs(){
-        return this.$store.getters.sublibs;
-      },
-      categories(){
-        return this.$store.getters.subjects;
-      },
-      clcs(){
-        return this.$store.getters.clcs;
-      },
       currentCategory(){
-        let category = this.$route.query.category || '';  
+        let category = this.$route.query.category || '';
         return category;
       },
       currentLib(){
-        let lib = this.$route.query.lib || '';  
+        let lib = this.$route.query.lib || '';
         return lib;
       },
       currentClc(){
-        let clc = this.$route.query.clc || '';  
+        let clc = this.$route.query.clc || '';
         return clc;
       },
       pageSize(){
-        let pageSize = this.$route.query.limit || 20;  
+        let pageSize = this.$route.query.limit || 20;
         return parseInt(pageSize);
       },
       currentPage(){
-        let currentPage = this.$route.query.page || 1;  
+        let currentPage = this.$route.query.page || 1;
         return parseInt(currentPage);
       }
     },
-    methods: { 
-      listBooksByCategory: function(category){
-        this.$router.push('/book/list?category=' + encodeURI(category));
-      },
-
-      listBooksByClc: function(clc){
-        this.$router.push('/book/list?clc=' + encodeURI(clc));
-      },
-
-      listBooksByLib: function(lib){
-        this.$router.push('/book/list?lib=' + encodeURI(lib));
-      },
-
+    methods: {
       createBook: function(){
         this.$router.push('/book/create');
       },
       uploadBook: function(){
         this.$router.push('/book/uploader');
       },
-      getLatestBooks: function(category,clc, lib, limit,page){   
-        this.$store.dispatch('getLatestBooks', {'category': category, 'clc': clc, 'lib': lib,'limit': limit, 'page': page}); 
+      getLatestBooks: function(category,clc, lib, limit,page){
+        this.$store.dispatch('getLatestBooks', {'category': category, 'clc': clc, 'lib': lib,'limit': limit, 'page': page});
       },
-      handleCurrentChange(val) {  
+      handleCurrentChange(val) {
         if(this.isSearchResult){
-          this.search(this.pageSize, val); 
+          this.search(this.pageSize, val);
         }else{
-          this.getLatestBooks(this.currentCategory, this.currentClc, this.currentLib, this.pageSize, val); 
+          this.getLatestBooks(this.currentCategory, this.currentClc, this.currentLib, this.pageSize, val);
         }
       },
       search: function(limit, page){
         let keywords = this.searchForm.keywords;
-        this.$store.dispatch('searchBooks', {'keywords': keywords, 'limit': limit, 'page': page});  
+        this.$store.dispatch('searchBooks', {'keywords': keywords, 'limit': limit, 'page': page});
       },
       // 加入收藏夹
       addToFav: function () {
--- src/store/modules/book.js
+++ src/store/modules/book.js
@@ -1,10 +1,10 @@
 import api from '../api/book';
 
 // initial state
-const state = { 
+const state = {
   hottestBooks: {
     'total': 0,
-    'data': [], 
+    'data': [],
     'isSearchResult': false,
   },
   latestBooks: {
@@ -19,23 +19,23 @@ const state = {
     'keywords': ''
   },
   bookDetail: {
-     _id: '', 
+     _id: '',
     name: '',
     ISBN: '',
     publishedAt: '',
     bookNo: '',
     recordNo: '',
-    categories: [],  
+    categories: [],
     summary: '',
-    pdf: '', 
+    pdf: '',
     epub: '',
     strTags: '',
     texts: [],
-    tags: [], 
+    tags: [],
     creator: '',
   },
   bookError: '',
-  bookSaveStatus: false, 
+  bookSaveStatus: false,
   bookDeleteStatus: false,
   bookCategoriesEditDialogVisible: false,
   bookSublibsEditDialogVisible: false,
@@ -48,164 +48,170 @@ const state = {
   savedBook: {
     id:'',
     title: ''
-  }, 
+  },
+  libMenuVisible: true,
+  categoryMenuVisible: true,
+  clcMenuVisible: true
 };
 
 // getters
 const getters = {
   hottestBooks: state => state.hottestBooks.data,
-  books: state => state.books.data, 
+  books: state => state.books.data,
   latestBooks: state => state.latestBooks.data,
   hottestBooksTotal: state => state.hottestBooks.total,
   booksTotal: state => state.books.total,
   latestBooksTotal: state => state.latestBooks.total,
-  bookDetail: state => state.bookDetail, 
+  bookDetail: state => state.bookDetail,
   savedBook: state => state.savedBook,
-  bookSaveStatus: state => state.bookSaveStatus,  
-  bookDeleteStatus: state => state.bookDeleteStatus,  
+  bookSaveStatus: state => state.bookSaveStatus,
+  bookDeleteStatus: state => state.bookDeleteStatus,
   bookDeleteResult: state => state.bookDeleteResult,
   bookDetailReady: state => state.bookDetailReady,
   bookCategoriesEditDialogVisible: state => state.bookCategoriesEditDialogVisible,
   bookSublibsEditDialogVisible: state => state.bookSublibsEditDialogVisible,
   bookClcsEditDialogVisible: state => state.bookClcsEditDialogVisible,
+  libMenuVisible: state => state.libMenuVisible,
+  categoryMenuVisible: state => state.categoryMenuVisible,
+  clcMenuVisible: state => state.clcMenuVisible
 };
 
 // actions
-const actions = { 
-  getHottestBooks ({ commit, state }, params) {  
+const actions = {
+  getHottestBooks ({ commit, state }, params) {
     let promise = api.getHottest(params.category, params.limit, params.page);
     promise.then((response) => {
       console.log(response.data);
-      commit('setHottestBooks', response.data); 
-      commit('setBooks', state.hottestBooks); 
+      commit('setHottestBooks', response.data);
+      commit('setBooks', state.hottestBooks);
     }, (response) => {
       console.log(response);
-      commit('setHottestBooks', []); 
-      commit('setBooks', []); 
+      commit('setHottestBooks', []);
+      commit('setBooks', []);
     });
-  }, 
-  getLatestBooks ({ commit,state }, params) {  
+  },
+  getLatestBooks ({ commit,state }, params) {
     let promise = api.getLatest(params.category,params.clc,params.lib, params.limit, params.page);
     promise.then((response) => {
       console.log(response.data);
-      commit('setLatestBooks', response.data); 
-      commit('setBooks', state.latestBooks); 
+      commit('setLatestBooks', response.data);
+      commit('setBooks', state.latestBooks);
     }, (response) => {
       console.log(response);
-      commit('setLatestBooks', []); 
+      commit('setLatestBooks', []);
       commit('setBooks', []);
     });
-  }, 
-  searchBooks ({ commit }, params) {  
+  },
+  searchBooks ({ commit }, params) {
     let promise = api.search(params.keywords, params.limit, params.page);
     promise.then((response) => {
-      console.log(response.data); 
-      let result = {'total': response.data.total, 
+      console.log(response.data);
+      let result = {'total': response.data.total,
                     'data': response.data.data,
                     'isSearchResult': true,
                     'keywords': params.keywords};
-      commit('setBooks', result); 
+      commit('setBooks', result);
     }, (response) => {
-      console.log(response); 
-      commit('setBooks', []);  
+      console.log(response);
+      commit('setBooks', []);
     });
-  }, 
-  getBookDetail({ commit,state }, params) { 
+  },
+  getBookDetail({ commit,state }, params) {
     commit('setBookDetailReady',false);
     let promise = api.getBookDetail(params.bookId);
     promise.then((response) => {
       //console.log(response);
-      commit('setBookDetail', response.data);  
+      commit('setBookDetail', response.data);
       commit('setBookDetailReady',true);
-    }, (response) => { 
+    }, (response) => {
       commit('setBookError', '专家不存在');
-      console.log(response); 
-    }); 
-  }, 
-  postBook ({ commit }, bookInfo) {   
+      console.log(response);
+    });
+  },
+  postBook ({ commit }, bookInfo) {
     commit('setBookSaveStatus', false);
     let promise = api.postBook(bookInfo);
     promise.then((response) => {
       console.log(response.data);
-      commit('setSavedBook', response.data); 
-      commit('setBookSaveStatus', true); 
+      commit('setSavedBook', response.data);
+      commit('setBookSaveStatus', true);
     }, (response) => {
       console.log(response);
-      commit('setBookError', '专家保存失败'); 
+      commit('setBookError', '专家保存失败');
     });
-  }, 
-  postBookCategory ({ commit }, categoryInfo) {   
+  },
+  postBookCategory ({ commit }, categoryInfo) {
     commit('setBookSaveStatus', false);
     let promise = api.postBookCategory(categoryInfo.bookId, categoryInfo.category);
     promise.then((response) => {
       console.log(response.data);
-      commit('setSavedBook', response.data); 
-      commit('setBookSaveStatus', true); 
+      commit('setSavedBook', response.data);
+      commit('setBookSaveStatus', true);
     }, (response) => {
       console.log(response);
-      commit('setBookError', '保存失败'); 
+      commit('setBookError', '保存失败');
     });
-  }, 
-  putBook ({ commit }, bookInfo) {   
+  },
+  putBook ({ commit }, bookInfo) {
     commit('setBookSaveStatus', false);
     let promise = api.putBook(bookInfo);
     promise.then((response) => {
       console.log(response.data);
-      commit('setSavedBook', response.data); 
-      commit('setBookSaveStatus', true); 
+      commit('setSavedBook', response.data);
+      commit('setBookSaveStatus', true);
     }, (response) => {
       console.log(response);
-      commit('setBookError', '修改失败'); 
+      commit('setBookError', '修改失败');
     });
   },
-  putBookCategory ({ commit }, categoryInfo) {   
+  putBookCategory ({ commit }, categoryInfo) {
     commit('setBookSaveStatus', false);
     let promise = api.putBookCategory(categoryInfo.bookId, categoryInfo.category);
     promise.then((response) => {
       console.log(response.data);
-      commit('setSavedBook', response.data); 
-      commit('setBookSaveStatus', true); 
+      commit('setSavedBook', response.data);
+      commit('setBookSaveStatus', true);
     }, (response) => {
       console.log(response);
-      commit('setBookError', '修改失败'); 
+      commit('setBookError', '修改失败');
     });
   },
-  moveToBookCategory ({ commit }, categoryInfo) {   
+  moveToBookCategory ({ commit }, categoryInfo) {
     commit('setBookSaveStatus', false);
     let promise = api.moveToBookCategory(categoryInfo.bookId, categoryInfo.category, categoryInfo.moveToId);
     promise.then((response) => {
       console.log(response.data);
-      commit('setSavedBook', response.data); 
-      commit('setBookSaveStatus', true); 
+      commit('setSavedBook', response.data);
+      commit('setBookSaveStatus', true);
     }, (response) => {
       console.log(response);
-      commit('setBookError', '修改失败'); 
+      commit('setBookError', '修改失败');
     });
   },
-  deleteBook ({ commit }, bookId) {   
+  deleteBook ({ commit }, bookId) {
     commit('setBookDeleteStatus', false);
-    commit('setBookDeleteResult', {bookId: bookId, status: false});  
+    commit('setBookDeleteResult', {bookId: bookId, status: false});
     let promise = api.deleteBook(bookId);
-    promise.then((response) => {  
+    promise.then((response) => {
       console.log(response.data);
-      commit('setBookDeleteStatus', true); 
-      commit('setBookDeleteResult', {bookId: bookId, status: true}); 
+      commit('setBookDeleteStatus', true);
+      commit('setBookDeleteResult', {bookId: bookId, status: true});
     }, (response) => {
       console.log(response);
-      commit('setBookError', '删除失败'); 
+      commit('setBookError', '删除失败');
     });
   },
-  deleteBookCategory ({ commit }, categoryInfo) {   
+  deleteBookCategory ({ commit }, categoryInfo) {
     commit('setBookDeleteStatus', false);
-    commit('setBookDeleteResult', {bookId: categoryInfo.bookId, status: false});  
+    commit('setBookDeleteResult', {bookId: categoryInfo.bookId, status: false});
     let promise = api.deleteBookCategory(categoryInfo.bookId, categoryInfo.categoryId);
-    promise.then((response) => {  
+    promise.then((response) => {
       console.log(response.data);
-      commit('setBookDeleteStatus', true); 
-      commit('setBookDeleteResult', {bookId: categoryInfo.bookId, status: true}); 
+      commit('setBookDeleteStatus', true);
+      commit('setBookDeleteResult', {bookId: categoryInfo.bookId, status: true});
     }, (response) => {
       console.log(response);
-      commit('setBookError', '删除失败'); 
+      commit('setBookError', '删除失败');
     });
   }
 };
@@ -217,88 +223,111 @@ const mutations = {
     state.hottestBooks.total = books.total;
     state.hottestBooks.data = books.data;
     state.hottestBooks.isSearchResult = false;
-  }, 
+  },
   setBooks (state, books) {
     // 变更状态
-    state.books = books; 
+    state.books = books;
   },
   setLatestBooks (state, books) {
     // 变更状态
     state.latestBooks.total = books.total;
     state.latestBooks.data = books.data;
     state.latestBooks.isSearchResult = false;
-  }, 
+  },
   setBookDetail(state, book) {
     // 变更状态
     if(book){
-      for(let k in book){ 
-        if(k === 'categories' || k === 'texts' || k === 'tags'){ 
+      for(let k in book){
+        if(k === 'categories' || k === 'texts' || k === 'tags'){
             state.bookDetail[k] = [];
-            for(let item of book[k]){ 
+            for(let item of book[k]){
               state.bookDetail[k].push(item);
             }
             if(k === 'tags' && Object.prototype.toString.call(book.tags)==='[object Array]'){
               state.bookDetail.strTags = book.tags.join(' ');
             }
           }else{
-             state.bookDetail[k] = book[k]; 
-          } 
+             state.bookDetail[k] = book[k];
+          }
       }
     }
   },
   setSavedBook(state, book) {
       // 变更状态
     state.savedBook.id = book.id;
-    state.savedBook.title = book.title; 
-  }, 
+    state.savedBook.title = book.title;
+  },
   setBookError(state, error) {
     // 变更状态
-    state.bookError = error; 
-  }, 
+    state.bookError = error;
+  },
   clearBookError(state){
     state.bookError = '';
-  }, 
+  },
   setBookSaveStatus(state, status) {
     // 变更状态
-    state.bookSaveStatus = status; 
+    state.bookSaveStatus = status;
   },
   setBookDeleteStatus(state, status) {
     // 变更状态
-    state.bookDeleteStatus = status; 
-  }, 
+    state.bookDeleteStatus = status;
+  },
   setBookDeleteResult(state, result) {
     // 变更状态
-    state.bookDeleteResult = result; 
+    state.bookDeleteResult = result;
   },
   setBookDetailReady(state, status) {
     // 变更状态
-    state.bookDetailReady = status; 
+    state.bookDetailReady = status;
   },
   showBookCategoriesEditDialog(state) {
     // 变更状态
-    state.bookCategoriesEditDialogVisible = true; 
+    state.bookCategoriesEditDialogVisible = true;
   },
   hideBookCategoriesEditDialog(state) {
     // 变更状态
-    state.bookCategoriesEditDialogVisible = false; 
+    state.bookCategoriesEditDialogVisible = false;
   },
   showBookSublibsEditDialog(state) {
     // 变更状态
-    state.bookSublibsEditDialogVisible = true; 
+    state.bookSublibsEditDialogVisible = true;
   },
   hideBookSublibsEditDialog(state) {
     // 变更状态
-    state.bookSublibsEditDialogVisible = false; 
+    state.bookSublibsEditDialogVisible = false;
   },
   showBookClcsEditDialog(state) {
     // 变更状态
-    state.bookClcsEditDialogVisible = true; 
+    state.bookClcsEditDialogVisible = true;
   },
   hideBookClcEditDialog(state) {
     // 变更状态
-    state.bookClcsEditDialogVisible = false; 
-  }
-
+    state.bookClcsEditDialogVisible = false;
+  },
+  showLibMenu(state) {
+    // 变更状态
+    state.libMenuVisible = true;
+  },
+  hideLibMenu(state) {
+    // 变更状态
+    state.libMenuVisible = false;
+  },
+  showCategoryMenu(state) {
+    // 变更状态
+    state.categoryMenuVisible = true;
+  },
+  hideCategoryMenu(state) {
+    // 变更状态
+    state.categoryMenuVisible = false;
+  },
+  showClcMenu(state) {
+    // 变更状态
+    state.clcMenuVisible = true;
+  },
+  hideClcMenu(state) {
+    // 变更状态
+    state.clcMenuVisible = false;
+  },
 };
 
 export default {
