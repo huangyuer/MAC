9199fbd2d9dd39d447a2f57fc85af6bb40a912ad
tolerious <tolerious@qq.com>
2017-12-08 17:09:07 +0800

    update code ...

--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -23,17 +23,11 @@ const mutations = {
   searchAll (state, data) {
     console.log(data)
     state.allPageBookList = []
-    let a = data.hits
+    let a = data.allData.hits
     for (var i = 0; i < a.length; i++) {
       let b = new bookItem()
       let t = a[i].inner_hits.bookchapters.hits.hits//这是一个highlight的数组
-      // console.log(t[0].highlight.content)
       let tt = t[0].highlight.content
-      var stt = ''
-      // for (var i = 0; i < tt.length; i++) {
-      //   stt += tt[i]
-      // }
-      // console.log(stt)
       b.publisher = a[i]._source.publisher
       b.chiefEditor = a[i]._source.chiefEditor
       b.isbn = a[i]._source.isbn
