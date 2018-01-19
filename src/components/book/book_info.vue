6d204f8a3d43783e7e411c8848ef6ea9ecd2996c
tongjitech <tongjitech@aliyun.com>
2018-01-19 11:26:45 +0800

    modified:   src/components/book/book_info.vue

--- src/components/book/book_info.vue
+++ src/components/book/book_info.vue
@@ -22,6 +22,9 @@
                 <p>出版日期：<span>{{bookDetail.publishedAt}}</span></p>
                 <p>印张数：<span>{{bookDetail.sheets}}</span></p>
                 <p>定价：<span>{{bookDetail.price}}</span></p>
+                <div class="btn-container">
+                  <div class="btn" @click="readPdf(bookDetail)">PDF阅读</div> <div @click="readHtml(bookDetail)" class="btn">HTML阅读</div>
+                </div>
               </dd>
             </dl>
           </div>
@@ -48,7 +51,27 @@
 
   </div>
 </template>
-
+<style>
+  .btn-container{margin-top: 15px;margin-bottom: 12px;}
+  .btn-container .btn{
+    height: 20px;
+    width: 65px;
+    color: #E37423;
+    border: 1px solid #E37423;
+    text-align: center;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    cursor: pointer;
+    font-size: 11px;
+    line-height: 20px;
+    vertical-align: middle;
+    display: inline-block;
+    border-radius: 3px;
+    margin-right: 10px;
+  }
+</style>
 <script>
   import searchComponent from '../public/searchComponent.vue'
   import navBar from '../public/nav_bar.vue'
@@ -89,6 +112,16 @@
     methods: {
       init: function(){
         this.$store.dispatch('getBookDetail', {'bookId': this.bookId});
+      },
+      readPdf: function(bookDetail){
+        let pdfUrl = 'http://47.98.32.49/pdfviewer/pc?pdf=' + bookDetail.pdf
+        console.log(pdfUrl)
+        window.location.href = pdfUrl
+      },
+      readHtml: function(bookDetail){
+        let htmlUrl = '/book/content/' + bookDetail._id + '/chapter?src=Text%2Fchapter1.html'
+        console.log(htmlUrl)
+        this.$router.push(htmlUrl)
       },
       handleNodeClick(data, node) {
         //
