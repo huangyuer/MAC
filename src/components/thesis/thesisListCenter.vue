928b1247fa67796b0e8101e9565a7f7f87e49546
Leo Cai <leo@LeodeMacBook-Pro.local>
2018-03-13 18:01:25 +0800

    修改首页等样式bug，add thesis/list

--- src/components/thesis/thesisListCenter.vue
+++ src/components/thesis/thesisListCenter.vue
@@ -4,26 +4,67 @@
       <li>
         <div class="thesis-item">
           <router-link to="/thesis/info/1">
-            <img src="../../assets/images/img.jpg"/>
             <div class="thesis-title">结构工程师</div>
-            <div class="thesis-time">2018年02期</div>
           </router-link>
+          <div class="thesis-subtitle">
+            <span class="thesis-category">期刊论文</span>
+            <span>作者名</span>
+            <span>-</span>
+            <span>期刊名字</span>
+            <span class="thesis-publish-id">CSTPCD</span>
+            <span>-</span>
+            <span class="thesis-publish-number">2017年1期</span>
+          </div>
+          <div class="thesis-summary">
+            内容摘要：为网易云课堂--运营微专业的课程作业，如有疑问，欢迎交流。 任选一互联网产品，策划一期活动，撰写出活动方案。 一、活动背景 常规节日活动，六一儿童节。 二、活动目的 获...
+            内容摘要：为网易云课堂--运营微专业的课程作业，如有疑问，欢迎交流。 任选一互联网产品，策划一期活动，撰写出活动方案。 一、活动背景 常规节日活动，六一儿童节。 二、活动目的 获...
+          </div>
+          <div class="thesis-keyword">
+            <span>关键词：</span>
+            <span>关键词一</span>
+            <span>关键词二</span>
+            <span>关键词三</span>
+          </div>
           <div class="thesis-item-btn-group">
             <span>在线阅读</span>
             <span>加入收藏</span>
           </div>
+          <div class="thesis-divider"></div>
         </div>
       </li>
 
       <li>
         <div class="thesis-item">
           <router-link to="/thesis/info/1">
-            <img src="../../assets/images/img.jpg"/>
             <div class="thesis-title">结构工程师</div>
-            <div class="thesis-time">2018年02期</div>
           </router-link>
+          <div class="thesis-subtitle">
+            <span class="thesis-category">期刊论文</span>
+            <span>作者名</span>
+            <span>-</span>
+            <span>期刊名字</span>
+            <span class="thesis-publish-id">CSTPCD</span>
+            <span>-</span>
+            <span class="thesis-publish-number">2017年1期</span>
+          </div>
+          <div class="thesis-summary">
+            内容摘要：为网易云课堂--运营微专业的课程作业，如有疑问，欢迎交流。 任选一互联网产品，策划一期活动，撰写出活动方案。 一、活动背景 常规节日活动，六一儿童节。 二、活动目的 获...
+            内容摘要：为网易云课堂--运营微专业的课程作业，如有疑问，欢迎交流。 任选一互联网产品，策划一期活动，撰写出活动方案。 一、活动背景 常规节日活动，六一儿童节。 二、活动目的 获...
+          </div>
+          <div class="thesis-keyword">
+            <span>关键词：</span>
+            <span>关键词一</span>
+            <span>关键词二</span>
+            <span>关键词三</span>
+          </div>
+          <div class="thesis-item-btn-group">
+            <span>在线阅读</span>
+            <span>加入收藏</span>
+          </div>
+          <div class="thesis-divider"></div>
         </div>
       </li>
+
     </ul>
   </div>
 </template>
