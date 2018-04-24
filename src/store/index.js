da5cd278036c9da025feec2561a8655c93feef21
tongjitech <tongjitech@aliyun.com>
2018-04-24 14:37:45 +0800

    add literature pdf reader

--- src/store/index.js
+++ src/store/index.js
@@ -35,6 +35,7 @@ Vue.use(Vuex)
 const state = {
   rootUrl: 'http://118.178.238.202:8000/',
   coverUrlPrefix: 'http://47.100.38.51:8244/',
+  OSS_PDF_SERVER_PREFIX: 'http://47.100.38.51:8080/pc/index.html?docuri=',
   BOOKS_PDF_SERVER_PREFIX: 'http://47.100.38.51:8080/pc/index.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
   MAGAZINES_PDF_SERVER_PREFIX: 'http://47.100.38.51:8080/pc/index.html?docuri=http://127.0.0.1:9090/magzines/', // 期刊pdf服务地址前缀
   IMAGE_SERVER_PREFIX: 'http://47.100.38.51:8244/' // 图片服务地址前缀
