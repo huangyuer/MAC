fd828efa54a8b7b3e0d918cb98bdc83c07351ec1
tongjitech <tongjitech@aliyun.com>
2018-04-27 17:45:04 +0800

    modified:   src/store/config.js

--- src/store/config.js
+++ src/store/config.js
@@ -5,8 +5,7 @@ console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV == 'development') {
   var config = {
     API_HOST: 'http://api.tongjix.cn/v1',
-    // API_HOST: 'http://101.132.93.53/v1',
-    rootUrl: 'http://118.178.238.202:8000/',
+    // API_HOST: 'http://101.132.93.53/v1', 
     coverUrlPrefix: 'http://47.100.38.51:8244/',
     BOOKS_PDF_SERVER_PREFIX: 'http://47.100.38.51:8080/pc/index.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
     BOOKS_PDF_MOBILE_SERVER_PREFIX: 'http://47.100.38.51:8080/mobile/index.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
