2e536461b6105b69f863b48f2e5d8e53dabf2f34
tongjitech <tongjitech@aliyun.com>
2018-05-23 10:52:54 +0800

    fix pdf server url

--- src/store/config.js
+++ src/store/config.js
@@ -7,10 +7,10 @@ if (process.env.NODE_ENV == 'development') {
     API_HOST: 'http://api.tongjix.cn/v1',
     // API_HOST: 'http://101.132.93.53/v1', 
     coverUrlPrefix: 'http://47.100.38.51:8244/',
-    BOOKS_PDF_SERVER_PREFIX: 'http://47.100.38.51:8080/pc/index.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
-    BOOKS_PDF_MOBILE_SERVER_PREFIX: 'http://47.100.38.51:8080/mobile/index.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
-    MAGAZINES_PDF_SERVER_PREFIX: 'http://47.100.38.51:8080/pc/index.html?docuri=http://127.0.0.1:9090/magzines/', // 期刊pdf服务地址前缀
-    MAGAZINES_PDF_MOBILE_SERVER_PREFIX: 'http://47.100.38.51:8080/mobile/index.html?docuri=http://127.0.0.1:9090/magzines/', // 期刊pdf服务地址前缀
+    BOOKS_PDF_SERVER_PREFIX: 'http://pdf.tongjix.cn/pc/viewer.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
+    BOOKS_PDF_MOBILE_SERVER_PREFIX: 'http://pdf.tongjix.cn/mobile/viewer.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
+    MAGAZINES_PDF_SERVER_PREFIX: 'http://pdf.tongjix.cn/pc/viewer.html?docuri=http://127.0.0.1:9090/magzines/', // 期刊pdf服务地址前缀
+    MAGAZINES_PDF_MOBILE_SERVER_PREFIX: 'http://pdf.tongjix.cn/mobile/viewer.html?docuri=http://127.0.0.1:9090/magzines/', // 期刊pdf服务地址前缀
     IMAGE_SERVER_PREFIX: 'http://47.100.38.51:8244/' // 图片服务地址前缀
   }
 } else {
@@ -18,10 +18,10 @@ if (process.env.NODE_ENV == 'development') {
     API_HOST: 'http://api.tongjix.cn/v1',
     // API_HOST: 'http://101.132.93.53/v1', 
     coverUrlPrefix: 'http://47.100.38.51:8244/',
-    BOOKS_PDF_SERVER_PREFIX: 'http://47.100.38.51:8080/pc/index.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
-    BOOKS_PDF_MOBILE_SERVER_PREFIX: 'http://47.100.38.51:8080/mobile/index.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
-    MAGAZINES_PDF_SERVER_PREFIX: 'http://47.100.38.51:8080/pc/index.html?docuri=http://127.0.0.1:9090/magzines/', // 期刊pdf服务地址前缀
-    MAGAZINES_PDF_MOBILE_SERVER_PREFIX: 'http://47.100.38.51:8080/mobile/index.html?docuri=http://127.0.0.1:9090/magzines/', // 期刊pdf服务地址前缀
+    BOOKS_PDF_SERVER_PREFIX: 'http://pdf.tongjix.cn/pc/viewer.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
+    BOOKS_PDF_MOBILE_SERVER_PREFIX: 'http://pdf.tongjix.cn/mobile/viewer.html?docuri=http://127.0.0.1:9090/books/', // 图书pdf服务地址前缀
+    MAGAZINES_PDF_SERVER_PREFIX: 'http://pdf.tongjix.cn/pc/viewer.html?docuri=http://127.0.0.1:9090/magzines/', // 期刊pdf服务地址前缀
+    MAGAZINES_PDF_MOBILE_SERVER_PREFIX: 'http://pdf.tongjix.cn/mobile/viewer.html?docuri=http://127.0.0.1:9090/magzines/', // 期刊pdf服务地址前缀
     IMAGE_SERVER_PREFIX: 'http://47.100.38.51:8244/' // 图片服务地址前缀
     // API_HOST: 'http://47.100.38.51:7878/v1'
     // API_HOST: 'http://118.178.238.202:9988/v1'
